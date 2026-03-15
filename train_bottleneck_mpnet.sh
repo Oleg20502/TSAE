5f926ed12140132c9af8b7531c6f08d1cc7657f1
Oleg20502 <oleg20502@gmail.com>
2026-03-15 22:17:38 +0300

    chore: update configs

--- train_bottleneck_mpnet.sh
+++ train_bottleneck_mpnet.sh
@@ -6,6 +6,6 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3"
 NP=4
 
 accelerate launch --num_processes $NP --config_file ./accel_configs/fp32_ds_s2.yaml \
-    scripts/train_bottleneck.py --config configs/train/ae_mpnet_fineweb.yaml
+    scripts/train_bottleneck.py --config configs/train/ae_mpnet_fineweb_nl_2_std_0.2_sl_1.yaml
 
 echo "done"
