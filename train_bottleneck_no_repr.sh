82dda9f2a8b6c3ed58247465792ac68eefcc89f4
Oleg20502 <oleg20502@gmail.com>
2026-03-23 01:53:24 +0300

    fix: update no repr training script

--- train_bottleneck_no_repr.sh
+++ train_bottleneck_no_repr.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
-export CUDA_VISIBLE_DEVICES="0,1,2,3"
-NP=4
+export CUDA_VISIBLE_DEVICES="0,1"
+NP=2
 
-accelerate launch --num_processes $NP --config_file ./accel_configs/fp32_ds_s2.yaml \
-    scripts/train_bottleneck_no_repr.py --config configs/train/ae_no_repr_fineweb.yaml
+accelerate launch --num_processes $NP --config_file ./accel_configs/bf16.yaml \
+    scripts/train_bottleneck_no_repr.py \
+    --config configs/train/ae_no_repr_fineweb.yaml
 
 echo "done"
